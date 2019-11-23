007c3f1f6a5f0b3d704cf00794f87c5eddd95388
BELLO ABDULMATEEN <abdulmateenbinbello@gmail.com>
2019-11-23 11:58:40 +0100

    End of section 4

--- RelateApp.API/Controllers/ValuesController.cs
+++ RelateApp.API/Controllers/ValuesController.cs
@@ -20,6 +20,7 @@ namespace RelateApp.API.Controllers
             _context = context;
         }
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
